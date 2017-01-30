67ee6683a55092eb139ab8f3fca68eb5e3130706
Renato Cavalcante <renatocavalcante93@hotmail.com>
2017-01-29 23:21:29 -0300

    Accepted

---
+++ URI_1003.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int x, y, soma;
+    cin >> x;
+    cin >> y;
+    soma = x + y;
+    
+    cout << "SOMA = " << soma << endl;
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
